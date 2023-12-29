8b1d75d7d8bc667394c080dd86afbcb2f50edd4f
Dazi-san <kenatilahun@Kenas-MacBook-Pro-2.local>
2023-12-29 16:46:46 -0600

    Navigation bar and mobile menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import './App.css';
+import NavBar from './Componets/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Kena's Website</h1>
-        <nav>
-          <button>Home</button>
-          <button>About</button>
-          <button>Contact</button>
-        </nav>
-      </header>
-      <main>
-        <p>This is the homepage of my website.</p>
-        {/* Add additional content here */}
-      </main>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path='/'/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
